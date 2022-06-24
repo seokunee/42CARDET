809dd1ad59f30a7d4832ffdac248c1c1636d009e
seokunee <xocoolree@gmail.com>
2022-06-24 18:59:54 +0900

    20220624

---
+++ so_long/includes/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 13:12:22 by seokchoi          #+#    #+#             */
+/*   Updated: 2022/06/24 15:26:11 by seokchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <mlx.h> /* MiniLibX */
+# include <stdlib.h> /* exit, malloc, free */
+# include <unistd.h>  /* write, close, read*/
+# include <fcntl.h> /* open */
+# include <string.h> /* strerror */
+# include <stdio.h> /* perror */
+
+#endif
