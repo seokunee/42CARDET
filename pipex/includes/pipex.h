027826d709c33aaaecb6635a68958b2dd1f2dc99
seokunee <xocoolree@gmail.com>
2022-07-18 00:06:45 +0900

    [pipex] execve, access 함수 사용법 익힘

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,17 +6,27 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 15:19:15 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/07/08 15:44:48 by seokchoi         ###   ########.fr       */
+/*   Updated: 2022/07/17 16:05:05 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef PIPEX_H
+#ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <stdio.h>
+# include <unistd.h> /* fork, execve */
+# include <fcntl.h>  /* open */
+# include <stdlib.h> 
+# include <stdio.h> 
+# include <stdio.h> 
+# include "libft.h"
+
+typedef struct s_data
+{
+	char *cmd1_path;
+	char *cmd2_path;
+	char **cmd1;
+	char **cmd2;
+} t_data;
+
 
 #endif
--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,8 +6,59 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 15:02:45 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/07/08 15:19:05 by seokchoi         ###   ########.fr       */
+/*   Updated: 2022/07/17 18:18:06 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int	get_cmd_access(char *cmd)
+{
+	if (access((const char *)cmd, X_OK) == -1)
+		return (0); // 에러면 0
+	return (1); // 성공이면 1
+}
+
+void	set_cmd_and_path(char **av, t_data *data)
+{
+	data->cmd1 = ft_split(av[2], ' ');
+	data->cmd2 = ft_split(av[3], ' ');
+	if (ft_strnstr(data->cmd1[0], "/bin/", 5) == 0)
+		data->cmd1_path = ft_strjoin("/bin/",  data->cmd1[0]);
+	if (ft_strnstr(data->cmd2[0], "/bin/", 5) == 0)
+		data->cmd2_path = ft_strjoin("/bin/", data->cmd2[0]);
+}
+
+void	print_envp(char **envp)
+{
+	int	i;
+	
+	i = 0;
+	while (envp[i])
+	{
+		printf("%s", envp[i]);
+		i++;
+	}
+}
+int	main(int ac, char **av, char **envp)
+{
+	t_data data;
+
+	if (ac != 5)
+		return (0);
+
+	print_envp(envp);
+	// 명령어 set
+	set_cmd_and_path(av, &data);
+
+	// 명령어 유무 및 실행 가능여부 확인
+	printf("%s\n" , data.cmd1_path);
+	printf("%s\n\n" , data.cmd2_path);
+
+	printf("cmd1 : %d\n", get_cmd_access(data.cmd1_path));
+	printf("cmd2 : %d\n", get_cmd_access(data.cmd2_path));
+
+	// 명령어 실행
+	execve(data.cmd1_path, data.cmd1, NULL);
+	execve(data.cmd2_path, data.cmd2, NULL);
+}
