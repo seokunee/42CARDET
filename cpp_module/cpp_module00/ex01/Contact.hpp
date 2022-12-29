d13d9082aee3339d0c464d6ef01537ef017a8cc5
seokunee <xocoolree@gmail.com>
2022-12-29 21:56:44 +0900

    cpp00 ex01진행중

--- cpp_module/cpp_module00/ex01/Contact.hpp
+++ cpp_module/cpp_module00/ex01/Contact.hpp
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/28 16:30:06 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/12/28 16:30:13 by seokchoi         ###   ########.fr       */
+/*   Created: 2022/12/29 16:23:35 by seokchoi          #+#    #+#             */
+/*   Updated: 2022/12/29 21:56:29 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <Contact.hpp>
+
+class Contact {
+	private:
+		Contact store[8];
+	public:
+		Contact();
+		~Contact();
+};
+
+#endif
--- cpp_module/cpp_module00/ex01/PhoneBook.hpp
+++ cpp_module/cpp_module00/ex01/PhoneBook.hpp
@@ -6,13 +6,22 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 02:08:27 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/12/26 21:56:13 by seokchoi         ###   ########.fr       */
+/*   Updated: 2022/12/29 21:56:30 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
 #include "PhoneBook.hpp"
 
 class PhoneBook {
 	private:
 		
+	public:
+		PhoneBook();
+		~PhoneBook();
 }
+
+#endif
