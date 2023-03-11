e7db64881eec24eb36337fea4cf1c5cfea42179a
seokunee <xocoolree@gmail.com>
2023-03-11 21:47:55 +0900

    [CPP02] ex01 진행중

--- cpp_module/cpp02/ex01/Fixed.cpp
+++ cpp_module/cpp02/ex01/Fixed.cpp
@@ -6,11 +6,12 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 19:56:27 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/03/11 20:46:39 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/03/11 21:47:48 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include "cmath"
 
 Fixed::Fixed(){
 	std::cout << "Default constructor called" << std::endl;
@@ -24,6 +25,16 @@ Fixed::~Fixed(){
 Fixed::Fixed(const Fixed &src) {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
+};
+
+Fixed::Fixed(int value){
+	std::cout << "Int constructor called" << std::endl;
+	_fixPoint = value << _bit;
+};
+
+Fixed::Fixed(float value){
+	std::cout << "Float constructor called" << std::endl;
+	_fixPoint = roundf(value * static_cast<int>(1 << _bit));
 };
 
 Fixed &Fixed::operator=(const Fixed &src){
@@ -41,3 +52,16 @@ void Fixed::setRawBits( int const raw ){
 	std::cout << "Copy assignment operator called" << std::endl;
 	_fixPoint = raw;
 };
+
+float Fixed::toFloat( void ) const{
+	return (_fixPoint );
+};
+
+int Fixed::toInt( void ) const{
+	return (_fixPoint >> _bit);
+};
+
+std::ostream &operator<<( std::ostream &o, Fixed const &fix ){
+	std::cout <<
+};
+
--- cpp_module/cpp02/ex01/Fixed.hpp
+++ cpp_module/cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 19:56:40 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/03/11 20:31:34 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/03/11 21:47:49 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,23 @@
 
 class Fixed {
 	private:
-		int	_fixPoint;
+		int	_fixPoint; // 10 * 2^8 +   = 101010000000
 		static const int _bit = 8;
-
+		// 4byte -> 32bit => 1bit 31 - 8 정수 , 8 소수
+	// 10.5 => 1010.10000000
 	public:
 		Fixed();
 		Fixed(const Fixed &src);
+		Fixed(int value);
+		Fixed(float value);
 		Fixed &operator=(const Fixed &src);
 		~Fixed();
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
+		float toFloat( void ) const;
+		int toInt( void ) const;
 };
+
+std::ostream &operator<<( std::ostream &o, Fixed const &i );
 
 #endif
