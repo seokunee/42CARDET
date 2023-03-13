8f8eea6f9ad627e46e7a98a32ff467499892a6ad
seokunee <xocoolree@gmail.com>
2023-03-13 20:51:09 +0900

    [CPP02] ex01 완료ex02 진행중

--- cpp_module/cpp02/ex02/Fixed.cpp
+++ cpp_module/cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 19:56:27 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/03/13 18:59:15 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/03/13 20:48:13 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@
 Fixed::Fixed(){
 	std::cout << "Default constructor called" << std::endl;
 	_fixPoint = 0;
-}
-
-Fixed::~Fixed(){
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &src) {
@@ -37,11 +33,9 @@ Fixed::Fixed(float value){
 	_fixPoint = roundf(value * static_cast<float>(1 << _bit));
 };
 
-Fixed &Fixed::operator=(const Fixed &src){
-	std::cout << "Copy assignment operator called" << std::endl;
-	_fixPoint = src.getRawBits();
-	return (*this);
-};
+Fixed::~Fixed(){
+	std::cout << "Destructor called" << std::endl;
+}
 
 int Fixed::getRawBits( void ) const {
 	std::cout << "getRawBits member function called" << std::endl;
@@ -58,9 +52,83 @@ float Fixed::toFloat( void ) const{
 };
 
 int Fixed::toInt( void ) const{
-	// std::cout << _fixPoint << ", ";
 	return (_fixPoint >> _bit);
 };
+
+Fixed &Fixed::operator=(const Fixed &src){
+	std::cout << "Copy assignment operator called" << std::endl;
+	_fixPoint = src.getRawBits();
+	return (*this);
+};
+
+bool	Fixed::operator==(const Fixed &src){
+	if (_fixPoint == src._fixPoint)
+		return (true);
+	return (false);
+};
+
+bool	Fixed::operator!=(const Fixed &src){
+	if (_fixPoint != src._fixPoint)
+		return (true);
+	return (false);
+};
+
+bool	Fixed::operator<(const Fixed &src){
+	if (_fixPoint < src._fixPoint)
+		return (true);
+	return (false);
+};
+
+bool	Fixed::operator>(const Fixed &src){
+	if (_fixPoint > src._fixPoint)
+		return (true);
+	return (false);
+};
+
+bool	Fixed::operator<=(const Fixed &src){
+	if (_fixPoint <= src._fixPoint)
+		return (true);
+	return (false);
+};
+
+bool	Fixed::operator>=(const Fixed &src){
+	if (_fixPoint >= src._fixPoint)
+		return (true);
+	return (false);
+};
+
+Fixed &Fixed::operator+(const Fixed &src){
+
+};
+
+Fixed &Fixed::operator-(const Fixed &src){
+
+};
+
+Fixed &Fixed::operator*(const Fixed &src){
+
+};
+
+Fixed &Fixed::operator/(const Fixed &src){
+
+};
+
+static  Fixed	&min(Fixed &_fix1, Fixed &_fix2){
+
+};
+
+static  Fixed	&max(Fixed &_fix1, Fixed &_fix2){
+
+};
+
+static const Fixed	&min(const Fixed &_fix1, const Fixed &_fix2){
+
+};
+
+static const Fixed	&max(const Fixed &_fix1, const Fixed &_fix2){
+
+};
+
 
 std::ostream &operator<<( std::ostream &o, Fixed const &fix ){
 	o << fix.toFloat();
--- cpp_module/cpp02/ex02/Fixed.hpp
+++ cpp_module/cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 19:56:40 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/03/13 19:45:14 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/03/13 20:35:33 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,37 @@ class Fixed {
 		Fixed(const Fixed &src);
 		Fixed(int value);
 		Fixed(float value);
-		Fixed &operator=(const Fixed &src);
+
 		~Fixed();
+
+		Fixed	&operator=(const Fixed &src);
+		bool	operator==(const Fixed &src);
+		bool	operator!=(const Fixed &src);
+		bool	operator<(const Fixed &src);
+		bool	operator>(const Fixed &src);
+		bool	operator<=(const Fixed &src);
+		bool	operator>=(const Fixed &src);
+
+		Fixed	&operator+(const Fixed &src);
+		Fixed	&operator-(const Fixed &src);
+		Fixed	&operator*(const Fixed &src);
+		Fixed	&operator/(const Fixed &src);
+
+		Fixed &operator++();
+		Fixed operator++(int);
+		Fixed &operator--();
+		Fixed operator--(int);
+
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
 		float toFloat( void ) const;
 		int toInt( void ) const;
+
+
+		static  Fixed	&min(Fixed &_fix1, Fixed &_fix2);
+		static  Fixed	&max(Fixed &_fix1, Fixed &_fix2);
+		static const Fixed	&min(const Fixed &_fix1, const Fixed &_fix2);
+		static const Fixed	&max(const Fixed &_fix1, const Fixed &_fix2);
 };
 
 std::ostream &operator<<( std::ostream &o, Fixed const &i );
