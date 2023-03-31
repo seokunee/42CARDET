2347366a00e68757b6e57f27c5c28a3ce06eacdf
seokunee <xocoolree@gmail.com>
2023-03-31 22:06:11 +0900

    [CPP05] ex00 완료

---
+++ cpp_module/cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 16:43:30 by seokchoi          #+#    #+#             */
+/*   Updated: 2023/03/31 22:02:19 by seokchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+class Bureaucrat {
+	private:
+		const std::string _name;
+		int	_grade;
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const Bureaucrat &src);
+		~Bureaucrat();
+		Bureaucrat &operator=(const Bureaucrat &src);
+
+		const std::string getName() const;
+		int getGrade() const;
+
+		void increment();
+		void decrement();
+};
+
+std::ostream &operator<<( std::ostream &o, const Bureaucrat &i );
+
+#endif
