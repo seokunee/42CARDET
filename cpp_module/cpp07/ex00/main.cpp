0b797b642a243fb3ddd1f9e5d7471431c40c4ef8
seokunee <xocoolree@gmail.com>
2023-04-15 19:07:48 +0900

    [CPP07] ex00 완료

---
+++ cpp_module/cpp07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 16:42:37 by seokchoi          #+#    #+#             */
+/*   Updated: 2023/04/15 19:07:27 by seokchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	return 0;
+}
---
+++ cpp_module/cpp07/ex00/whatever.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 16:22:54 by seokchoi          #+#    #+#             */
+/*   Updated: 2023/04/15 19:07:15 by seokchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &x, T &y)
+{
+	T tmp;
+	tmp = y;
+	y = x;
+	x = tmp;
+}
+
+template <typename T>
+T &min(T &x, T &y)
+{
+	if (x < y)
+		return (x);
+	return (y);
+}
+
+template <typename T>
+T &max(T &x, T &y)
+{
+	if (x <= y)
+		return (y);
+	return (x);
+}
+
+#endif
