8e0dd26a8f6d46023e2d8d1c797d15b801d50c6c
seokunee <xocoolree@gmail.com>
2023-04-17 17:18:32 +0900

    error fix 및 임시저장

--- cpp_module/cpp03/ex02/ClapTrap.hpp
+++ cpp_module/cpp03/ex02/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 19:56:10 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/04/07 20:15:25 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/04/17 16:55:02 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 
 #include <iostream>
 
-class ClapTrap {
-	protected:
-		std::string _name;
-		unsigned int _hit;
-		unsigned int _power;
-		unsigned int _attackDamage;
-	public:
-		ClapTrap();
-		ClapTrap(std::string name);
-		~ClapTrap();f
-		ClapTrap(ClapTrap const &src);
+class ClapTrap
+{
+protected:
+	std::string _name;
+	unsigned int _hit;
+	unsigned int _power;
+	unsigned int _attackDamage;
 
-		ClapTrap &operator=(const ClapTrap &src);
-		virtual void attack(const std::string& target);
-		void takeDamage(unsigned int amount);
-		void beRepaired(unsigned int amount);
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	~ClapTrap();
+	ClapTrap(ClapTrap const &src);
+
+	ClapTrap &operator=(const ClapTrap &src);
+	virtual void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 };
 
 #endif
---
+++ cpp_module/cpp07/ex02/Array.hpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 16:22:54 by seokchoi          #+#    #+#             */
+/*   Updated: 2023/04/17 01:31:08 by seokchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+	T *_array;
+	unsigned int _size;
+
+public:
+	Array() : _array(NULL), _size(0){};
+
+	Array(unsigned int n) : _array(new T(n)), _size(n){};
+
+	Array(const Array<T> &src)
+	{
+		if (this == &src)
+			return;
+		*this = src;
+	};
+
+	Array<T> &operator=(const Array<T> &src)
+	{
+		if (this == &src)
+			return (*this);
+		for (unsigned int i = 0; i < src.size(); i++)
+		{
+			_array[i] = src[i];
+		}
+		return (*this);
+	}
+
+	~Array()
+	{
+		if (_array != NULL)
+			delete[] _array;
+	};
+
+	class OutOfBound : public std::exception
+	{
+	public:
+		const char *what() const throw()
+		{
+			return ("Array index out of bound exception");
+		};
+	};
+
+	T &operator[](unsigned int n)
+	{
+		if (n >= _size)
+			throw(OutOfBound());
+		return _array[n];
+	};
+
+	const T &operator[](unsigned int n) const
+	{
+		if (n >= _size)
+			throw(OutOfBound());
+		return _array[n];
+	};
+
+	unsigned int size() const
+	{
+		return _size;
+	};
+};
+
+#endif
