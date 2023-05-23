6ce1eca51414e13fbac19ca4325596df680f513a
seokunee <xocoolree@gmail.com>
2023-05-23 15:49:40 +0900

    [fix CPP01] ex04 -> s1가 s2에 포함될 경우 고려 못함.

--- cpp_module/cpp01/ex04/Sed.cpp
+++ cpp_module/cpp01/ex04/Sed.cpp
@@ -6,24 +6,28 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 17:25:25 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/03/09 00:38:03 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/05/23 15:48:21 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
 
-Sed::Sed(){}
+Sed::Sed() {}
 
-Sed::~Sed(){}
+Sed::~Sed() {}
 
-void Sed::_read(std::string &line){
+void Sed::_read(std::string &line)
+{
 	_readFile.open(_file);
-	if (_readFile.is_open() == true){
-		while (std::getline(_readFile, _buf)){
+	if (_readFile.is_open() == true)
+	{
+		while (std::getline(_readFile, _buf))
+		{
 			line.append(_buf);
 			line += '\n';
 		}
-		if (!_readFile.eof()) {
+		if (!_readFile.eof())
+		{
 			_readFile.close();
 			throw_error("There was an error reading " + _file + " file.");
 		}
@@ -33,27 +37,34 @@ void Sed::_read(std::string &line){
 	_readFile.close();
 }
 
-void Sed::_write(std::string &line){
+void Sed::_write(std::string &line)
+{
 	_writeFile.open(_new_file);
-	if (_writeFile.is_open() == true){
+	if (_writeFile.is_open() == true)
+	{
 		_writeFile << line;
-	} else {
+	}
+	else
+	{
 		throw_error("Can not open " + _new_file);
 	}
 	_writeFile.close();
 }
 
-void Sed::_changeWord(std::string &line){
+void Sed::_changeWord(std::string &line)
+{
 	size_t idx;
 
-	while (line.find(_s1) != std::string::npos){
+	while (line.find(_s1) != std::string::npos)
+	{
 		idx = line.find(_s1);
 		line.erase(idx, _s1.length());
 		line.insert(idx, _s2);
 	}
 }
 
-void Sed::toDoLikeSed(std::string file, std::string s1, std::string s2) {
+void Sed::toDoLikeSed(std::string file, std::string s1, std::string s2)
+{
 	_file = file;
 	_s1 = s1;
 	_s2 = s2;
@@ -62,6 +73,7 @@ void Sed::toDoLikeSed(std::string file, std::string s1, std::string s2) {
 	if (_file == "" || _s1 == "")
 		throw_error("file or s1 is empty");
 	_read(line);
-	_changeWord(line);
+	if (s1 != s2) // 이것을 보는 사람이 있거든 s2에 s1이 포함되는지 확인하시오. ex. s1 = 123, s2=123123 -> 이 경우 무한 루프가 일어난다.
+		_changeWord(line);
 	_write(line);
 }
