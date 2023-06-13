13649c8712c975e95cc85e5c433a731be5dfd4a5
seokunee <xocoolree@gmail.com>
2023-06-13 20:53:29 +0900

    [refactor] main 정리

--- cpp_module/cpp05/ex02/main.cpp
+++ cpp_module/cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 16:42:37 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/05/16 15:21:07 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/13 20:21:45 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,53 +20,29 @@ int main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 	{
-		try
-		{
-			Bureaucrat seok("seok", 3);
-			// Bureaucrat 					seok("seok", 50);
-			PresidentialPardonForm atom("atom");
-			seok.signForm(atom);
-			seok.executeForm(atom);
-		}
-		catch (std::exception &error)
-		{
-			std::cout << "ERROR!!" << std::endl;
-			std::cout << error.what() << std::endl;
-		}
+		Bureaucrat seok("seok", 3);
+		// Bureaucrat 			seok("seok", 50);
+		PresidentialPardonForm atom("atom");
+		seok.signForm(atom);
+		seok.executeForm(atom);
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 	{
-		try
-		{
-			Bureaucrat choi("choi", 3);
-			// Bureaucrat 			choi("choi", 50);
-			RobotomyRequestForm atom("atom");
-			choi.signForm(atom);
-			choi.executeForm(atom);
-		}
-		catch (std::exception &error)
-		{
-			std::cout << "ERROR!!" << std::endl;
-			std::cout << error.what() << std::endl;
-		}
+		Bureaucrat choi("choi", 3);
+		// Bureaucrat 			choi("choi", 50);
+		RobotomyRequestForm atom("atom");
+		choi.signForm(atom);
+		choi.executeForm(atom);
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 	{
-		try
-		{
-			Bureaucrat hun("hun", 3);
-			// Bureaucrat 			hun("hun", 140);
-			ShrubberyCreationForm atom("atom");
-			hun.signForm(atom);
-			hun.executeForm(atom);
-		}
-		catch (std::exception &error)
-		{
-			std::cout << "ERROR!!" << std::endl;
-			std::cout << error.what() << std::endl;
-		}
+		Bureaucrat hun("hun", 3);
+		// Bureaucrat 			hun("hun", 140);
+		ShrubberyCreationForm atom("atom");
+		hun.signForm(atom);
+		hun.executeForm(atom);
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
