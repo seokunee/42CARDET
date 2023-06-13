06c020358156f273f61951e4c2f60b5c2af7d77e
seokunee <xocoolree@gmail.com>
2023-06-13 19:34:29 +0900

    [refactor] 필요없는 쇼  try, catch 삭제

--- cpp_module/cpp05/ex01/main.cpp
+++ cpp_module/cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 16:42:37 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/06/13 16:24:45 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/13 18:20:40 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,22 @@ int main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 	{
-		try
-		{
-			Bureaucrat seok("seok", 3);
-			Form atom("atom", 15, 20);
-			std::cout << seok;
-			std::cout << atom;
-			seok.signForm(atom);
-			std::cout << atom;
-		}
-		catch (std::exception &error)
-		{
-			std::cout << "ERROR!!" << std::endl;
-			std::cout << error.what() << std::endl;
-		}
+		Bureaucrat seok("seok", 3);
+		Form atom("atom", 15, 20);
+		std::cout << seok;
+		std::cout << atom;
+		seok.signForm(atom);
+		std::cout << atom;
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 	{
-		try
-		{
-			Bureaucrat choi("choi", 18);
-			Form atom("atom", 15, 20);
-			std::cout << choi;
-			std::cout << atom;
-			choi.signForm(atom);
-			std::cout << atom;
-		}
-		catch (std::exception &error)
-		{
-			std::cout << "ERROR!!" << std::endl;
-			std::cout << error.what() << std::endl;
-		}
+		Bureaucrat choi("choi", 18);
+		Form atom("atom", 15, 20);
+		std::cout << choi;
+		std::cout << atom;
+		choi.signForm(atom);
+		std::cout << atom;
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
