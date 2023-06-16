21fff4378f144b0afc6971fbe2d74b7d77e846ec
seokunee <xocoolree@gmail.com>
2023-06-16 15:07:09 +0900

    [cpp07] ex01 코드 보강

--- cpp_module/cpp07/ex01/iter.hpp
+++ cpp_module/cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 16:22:54 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/04/16 20:48:40 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/16 14:57:03 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 #include <iostream>
 
 template <typename T, typename Func>
-void iter(T *array, int len, Func function)
+void iter(T *array, size_t len, Func &function)
 {
-	for (int i = 0; i < len; i++)
+	for (size_t i = 0; i < len; i++)
 	{
 		function(array[i]);
 	}
--- cpp_module/cpp07/ex01/main.cpp
+++ cpp_module/cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 16:42:37 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/04/16 20:47:52 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/16 15:04:10 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@ int main(void)
 	double d[5] = {0.0, 1.1, 2.2, 3.3, 4.4};
 	char c[6] = {'a', 'b', 'c', 'd', 'e', 'f'};
 
-	::iter(array, 3, print<std::string>);
-	::iter(i, 5, print<int>);
-	::iter(d, 5, print<double>);
-	::iter(c, 6, print<char>);
+	if (sizeof(array) > 0)
+		::iter(array, static_cast<size_t>(sizeof(array) / sizeof(array[0])), ::print<std::string>);
+	if (sizeof(i) > 0)
+		::iter(i, static_cast<size_t>(sizeof(i) / sizeof(i[0])), ::print<int>);
+	if (sizeof(d) > 0)
+		::iter(d, static_cast<size_t>(sizeof(d) / sizeof(d[0])), ::print<double>);
+	if (sizeof(c) > 0)
+		::iter(c, static_cast<size_t>(sizeof(c) / sizeof(c[0])), ::print<char>);
 	return 0;
 }
