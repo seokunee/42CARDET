8844e65609a10eb7578e3147333494bf87e1c040
seokunee <xocoolree@gmail.com>
2023-06-19 14:26:48 +0900

    [cpp08/ex02] 임시저장

---
+++ cpp_module/cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 03:14:10 by seokchoi          #+#    #+#             */
+/*   Updated: 2023/06/19 14:26:48 by seokchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+template <typename T>
+class MutantStack
+{
+private:
+public:
+	void push();
+	void pop();
+};
+
+#endif
