0dbd9241349eabc393775a3d481970ce747640d5
seokunee <xocoolree@gmail.com>
2023-06-21 03:51:51 +0900

    [cpp08/ex02] 완성

--- cpp_module/cpp08/ex02/MutantStack.hpp
+++ cpp_module/cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 03:14:10 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/06/19 18:20:34 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/21 03:45:57 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,40 @@
 
 #include <iostream>
 #include <stack>
-#include <deque>
 
-// template <typename T>
-// class MutantStack : public std::stack<T>
-// {
-// private:
-// 	typename T _mutantStack;
-
-// public:
-// 	void push(typename T::value_type i)
-// 	{
-// 		_mutantStack.push(i);
-// 	};
-
-// 	void pop()
-// 	{
-// 		_mutantStack.pop();
-// 	};
-
-// 	typename T::iterator begin(){
-
-// 	};
-// 	typename T::iterator end(){
-
-// 	};
-// };
-
-// template <typename T>
-// class MutantStack : public std::stack<T>
-// {
-// public:
-// 	typename Container::iterator begin() { return this->c.begin(); }
-// 	typename Container::iterator end() { return this->c.end(); }
-// };
-
-template <typename T, typename Container = std::deque<T>>
-class MutantStack : public std::stack<T, Container>
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 public:
-	using typename std::stack<T, Container>::container_type;
-	using typename container_type::iterator;
+	MutantStack(){};
+	~MutantStack(){};
+	MutantStack(const MutantStack &src)
+	{
+		*this = src;
+	}
+	MutantStack &operator=(const MutantStack &src)
+	{
+		if (this != &src)
+			*this = src;
+		return *this;
+	}
+
+	typedef typename MutantStack<T>::stack::container_type::iterator iterator;
+	typedef typename MutantStack<T>::stack::container_type::const_iterator const_iterator;
+	typedef typename MutantStack<T>::stack::container_type::reverse_iterator reverse_iterator;
+	typedef typename MutantStack<T>::stack::container_type::const_reverse_iterator const_reverse_iterator;
 
 	iterator begin() { return this->c.begin(); }
 	iterator end() { return this->c.end(); }
+
+	const_iterator cbegin() { return this->c.cbegin(); }
+	const_iterator cend() { return this->c.cend(); }
+
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
+
+	const_reverse_iterator crbegin() { return this->c.crbegin(); }
+	const_reverse_iterator crend() { return this->c.crend(); }
 };
 
 #endif
--- cpp_module/cpp08/ex02/main.cpp
+++ cpp_module/cpp08/ex02/main.cpp
@@ -6,33 +6,61 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 02:34:57 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/06/19 18:20:46 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/21 03:51:10 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <list>
 #include "MutantStack.hpp"
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	{
+		std::list<int> list;
+		list.push_back(5);
+		list.push_back(17);
+		std::cout << list.back() << std::endl;
+		list.pop_back();
+		std::cout << list.size() << std::endl;
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+		list.push_back(0);
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> l(list);
+	}
 	return 0;
 }
