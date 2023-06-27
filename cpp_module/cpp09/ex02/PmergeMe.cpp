26a9ea444a3926453cc3b5f38204c201c50fbfda
seokunee <xocoolree@gmail.com>
2023-06-27 18:20:55 +0900

    [CPP09/ex02] deque를 이용한 sort 완료

--- cpp_module/cpp09/ex02/PmergeMe.cpp
+++ cpp_module/cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 16:56:45 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/06/26 20:03:09 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/27 18:12:42 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,11 +56,33 @@ void PmergeMe::setArray(int ac, char **av)
 		_deque.push_back(value);
 	}
 }
-void printList(std::list<int> &list, std::string when)
+
+void PmergeMe::printList(std::list<int> &list, std::string when)
 {
 	std::cout << when << ":" << std::setw(10 - when.length());
+	if (list.size() > 5)
+		for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+		{
+			std::cout << *it << " ";
+		}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printListUnderFive(std::list<int> &list, std::string when)
+{
+	bool stop = false;
+	int count = 0;
+	std::cout << when << ":" << std::setw(10 - when.length());
+	if (list.size() > 5)
+		stop = true;
 	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
 	{
+		count++;
+		if (stop && count == 5)
+		{
+			std::cout << "[...]";
+			break;
+		}
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
@@ -81,7 +103,7 @@ void PmergeMe::printlistlist(std::list<INT_LIST> &list)
 	std::cout << "]" << std::endl;
 }
 
-void PmergeMe::merge(std::list<INT_LIST> &list, int left, int mid, int right)
+void PmergeMe::listMerge(std::list<INT_LIST> &list, int left, int mid, int right)
 {
 	int n1 = mid - left + 1;
 	int n2 = right - mid;
@@ -143,19 +165,19 @@ void PmergeMe::merge(std::list<INT_LIST> &list, int left, int mid, int right)
 	}
 }
 
-void PmergeMe::fordJohnsonMergeSort(std::list<INT_LIST> &list, int left, int right)
+void PmergeMe::listFordJohnsonMergeSort(std::list<INT_LIST> &list, int left, int right)
 {
 	if (left < right)
 	{
 		int mid = left + (right - left) / 2;
-		fordJohnsonMergeSort(list, left, mid);
-		fordJohnsonMergeSort(list, mid + 1, right);
+		listFordJohnsonMergeSort(list, left, mid);
+		listFordJohnsonMergeSort(list, mid + 1, right);
 
-		merge(list, left, mid, right);
+		listMerge(list, left, mid, right);
 	}
 }
 
-void PmergeMe::fordJohnsonInsertSort(std::list<INT_LIST> &list)
+void PmergeMe::listFordJohnsonInsertSort(std::list<INT_LIST> &list)
 {
 	std::list<INT_LIST>::iterator it = list.begin();
 
@@ -211,7 +233,7 @@ void PmergeMe::list_sort()
 	INT_LIST tmp;
 	INT_LIST::iterator it = _list.begin();
 	std::list<INT_LIST> list_tmp;
-	for (size_t i = 0; i < _list.size(); i++) // 두개씩 짝짓고 그 안에 정렬하기
+	for (size_t i = 0; i < _list.size(); i++)
 	{
 		if (i + 2 < _list.size())
 		{
@@ -234,36 +256,235 @@ void PmergeMe::list_sort()
 		list_tmp.push_back(tmp);
 		tmp.clear();
 	}
-	fordJohnsonMergeSort(list_tmp, 0, list_tmp.size() - 1);
+	listFordJohnsonMergeSort(list_tmp, 0, list_tmp.size() - 1);
 	_list.clear();
-	// printlistlist(list_tmp);
-	fordJohnsonInsertSort(list_tmp);
+	listFordJohnsonInsertSort(list_tmp);
+}
+
+// --- ---  ---  ---  ---  ---  ---  ---  ---  ---  ---  ---  ---
+
+void PmergeMe::printDeque(std::deque<int> &deque, std::string when)
+{
+	std::cout << when << ":" << std::setw(10 - when.length());
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printDequeUnderFive(std::deque<int> &deque, std::string when)
+{
+	bool stop = false;
+	int count = 0;
+	std::cout << when << ":" << std::setw(10 - when.length());
+	if (deque.size() > 5)
+		stop = true;
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); ++it)
+	{
+		count++;
+		if (stop && count == 5)
+		{
+			std::cout << "[...]";
+			break;
+		}
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printdequedeque(std::deque<INT_DEQUE> &deque)
+{
+	std::cout << "array = [";
+	for (std::deque<INT_DEQUE>::iterator i = deque.begin(); i != deque.end(); ++i)
+	{
+		std::cout << "[";
+		for (INT_DEQUE::iterator j = (*i).begin(); j != (*i).end(); ++j)
+		{
+			std::cout << *j << ",";
+		}
+		std::cout << "],";
+	}
+	std::cout << "]" << std::endl;
+}
+
+void PmergeMe::dequeMerge(std::deque<INT_DEQUE> &deque, int left, int mid, int right)
+{
+	int n1 = mid - left + 1;
+	int n2 = right - mid;
+
+	std::deque<INT_DEQUE> leftArr, rightArr;
+	std::deque<INT_DEQUE>::iterator it = deque.begin();
+
+	std::advance(it, left);
+	for (int i = 0; i < n1; ++i)
+	{
+		leftArr.push_back(*it);
+		++it;
+	}
+	it = deque.begin();
+	std::advance(it, mid + 1);
+	for (int j = 0; j < n2; ++j)
+	{
+		rightArr.push_back(*it);
+		++it;
+	}
+
+	int i = 0, j = 0;
+	std::deque<INT_DEQUE>::iterator it_l, it_r;
+	it_l = leftArr.begin();
+	it_r = rightArr.begin();
+	it = deque.begin();
+	std::advance(it, left);
+	while (i < n1 && j < n2)
+	{
+		if (*(*it_l).begin() > *(*it_r).begin())
+		{
+			*it = *it_l;
+			++i;
+			++it_l;
+		}
+		else
+		{
+			*it = *it_r;
+			++j;
+			++it_r;
+		}
+		++it;
+	}
+
+	while (i < n1)
+	{
+		*it = *it_l;
+		++i;
+		++it_l;
+		++it;
+	}
+
+	while (j < n2)
+	{
+		*it = *it_r;
+		++j;
+		++it_r;
+		++it;
+	}
+}
+
+void PmergeMe::dequeFordJohnsonMergeSort(std::deque<INT_DEQUE> &deque, int left, int right)
+{
+	if (left < right)
+	{
+		int mid = left + (right - left) / 2;
+		dequeFordJohnsonMergeSort(deque, left, mid);
+		dequeFordJohnsonMergeSort(deque, mid + 1, right);
+
+		dequeMerge(deque, left, mid, right);
+	}
+}
+
+void PmergeMe::dequeFordJohnsonInsertSort(std::deque<INT_DEQUE> &deque)
+{
+	std::deque<INT_DEQUE>::iterator it = deque.begin();
+
+	_deque.push_back((*it).back());
+	if ((*it).size() == 2)
+		_deque.push_back((*it).front());
+
+	for (it = deque.end(); it != deque.begin(); --it)
+	{
+		if (it == deque.end())
+			continue;
+		if (it == deque.begin())
+			break;
+		if ((*it).size() == 1)
+			continue;
+		int insertNum = (*it).back();
+		std::deque<int>::reverse_iterator i;
+		for (i = _deque.rbegin(); i != _deque.rend(); ++i)
+		{
+			if (*i <= insertNum)
+			{
+				_deque.insert(i.base(), insertNum);
+				break;
+			}
+		}
+		if (i == _deque.rend())
+			_deque.push_front(insertNum);
+	}
+	for (it = deque.end(); it != deque.begin(); --it)
+	{
+		if (it == deque.end())
+			continue;
+		if (it == deque.begin())
+			break;
+		int insertNum = (*it).front();
+		std::deque<int>::reverse_iterator i;
+		for (i = _deque.rbegin(); i != _deque.rend(); ++i)
+		{
+			if (*i <= insertNum)
+			{
+				_deque.insert(i.base(), insertNum);
+				break;
+			}
+		}
+		if (i == _deque.rend())
+			_deque.push_front(insertNum);
+	}
+}
+
+void PmergeMe::deque_sort()
+{
+	size_t first, second;
+	INT_DEQUE tmp;
+	INT_DEQUE::iterator it = _deque.begin();
+	std::deque<INT_DEQUE> deque_tmp;
+	for (size_t i = 0; i < _deque.size(); i++)
+	{
+		if (i + 2 < _deque.size())
+		{
+			first = *it++;
+			second = *it++;
+			if (first < second)
+			{
+				tmp.push_back(second);
+				tmp.push_back(first);
+			}
+			else
+			{
+				tmp.push_back(first);
+				tmp.push_back(second);
+			}
+			i++;
+		}
+		else
+			tmp.push_back(*it++);
+		deque_tmp.push_back(tmp);
+		tmp.clear();
+	}
+	dequeFordJohnsonMergeSort(deque_tmp, 0, deque_tmp.size() - 1);
+	_deque.clear();
+	dequeFordJohnsonInsertSort(deque_tmp);
 }
 
 void PmergeMe::start()
 {
-	printList(_list, "Before");
+	// printList(_list, "Before");
+	printListUnderFive(_list, "Before");
 	clock_t start = clock();
 	list_sort();
 	clock_t end = clock();
-	printList(_list, "After");
+	// printList(_list, "After");
+	printListUnderFive(_list, "After");
 	double elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
-	std::cout << "Time to process a range of " << _list.size() << "  elements with std::[..] : " << elapsedTime << " us" << std::endl;
-}
+	std::cout << "Time to process a range of " << _list.size() << "  elements with std::list : " << elapsedTime << " us" << std::endl;
 
-/* Ford-Johnson 알고리즘 순서
- * 1단계 재귀
- * 1. 2개씩 숫자들을 묶는다. (4 2) (5 8) (7 9) (1)
- * 2. 같이 묶인 애들 2개만 sort 해준다. (4 2) (8 5) (9 7) (1)
- * 3. 묶은 애들 중에서 큰 애들을 기준으로 sort를 한번 해준다. => 이때 sort는 어떻게 하지? 버블?
- * (9 7) (8 5) (4 2) (1)
- *
- * 2단계 삽입
- * 1. 오른쪽부터 pend element를 삽입 정렬을 시킨다.
- * 2  이때 Jacobsthal number을 잘 계산해서 해줘야한다.
- * 3.
- *
- *
- * Jacobsthal number
- * 0 1 1 3 5 11 21
- */
+	// printDeque(_deque, "Before");
+	// printDequeUnderFive(_deque, "Before");
+	start = clock();
+	deque_sort();
+	end = clock();
+	// printDequeUnderFive(_deque, "After");
+	// printDeque(_deque, "After");
+	elapsedTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
+	std::cout << "Time to process a range of " << _deque.size() << "  elements with std::deque : " << elapsedTime << " us" << std::endl;
+}
--- cpp_module/cpp09/ex02/PmergeMe.hpp
+++ cpp_module/cpp09/ex02/PmergeMe.hpp
@@ -6,13 +6,14 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 16:56:55 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/06/26 19:37:15 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/06/26 21:48:50 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 #define INT_LIST std::list<int>
+#define INT_DEQUE std::deque<int>
 
 #include <iostream>
 #include <vector>
@@ -28,10 +29,22 @@ private:
 	std::deque<int> _deque;
 	void setArray(int ac, char **av);
 	void checkArgument(std::string str);
+
+	// list
+	void printList(std::list<int> &list, std::string when);
+	void printListUnderFive(std::list<int> &list, std::string when);
 	void printlistlist(std::list<INT_LIST> &list);
-	void merge(std::list<INT_LIST> &list, int left, int mid, int right);
-	void fordJohnsonMergeSort(std::list<INT_LIST> &list, int left, int right);
-	void fordJohnsonInsertSort(std::list<INT_LIST> &list);
+	void listMerge(std::list<INT_LIST> &list, int left, int mid, int right);
+	void listFordJohnsonMergeSort(std::list<INT_LIST> &list, int left, int right);
+	void listFordJohnsonInsertSort(std::list<INT_LIST> &list);
+
+	// deque
+	void printDeque(std::deque<int> &deque, std::string when);
+	void printDequeUnderFive(std::deque<int> &deque, std::string when);
+	void printdequedeque(std::deque<INT_DEQUE> &deque);
+	void dequeMerge(std::deque<INT_DEQUE> &deque, int left, int mid, int right);
+	void dequeFordJohnsonMergeSort(std::deque<INT_DEQUE> &deque, int left, int right);
+	void dequeFordJohnsonInsertSort(std::deque<INT_DEQUE> &deque);
 
 	PmergeMe();
 
