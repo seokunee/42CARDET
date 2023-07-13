6600c5b37d1c2684ddddbe8906f6623a80a1c990
seokunee <xocoolree@gmail.com>
2023-07-14 00:03:31 +0900

    [CPP08/ex00] refactoring

--- cpp_module/cpp08/ex00/easyfind.hpp
+++ cpp_module/cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 15:55:46 by seokchoi          #+#    #+#             */
-/*   Updated: 2023/06/18 17:22:49 by seokchoi         ###   ########.fr       */
+/*   Updated: 2023/07/14 00:03:14 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,6 @@
 template <typename T>
 typename T::iterator easyfind(T &con, int val)
 {
-	// if (!std::is_same<typename T::value_type, int>::value)
-	// 	throw std::runtime_error("value type is not Int!");
 	typename T::iterator it = std::find(con.begin(), con.end(), val);
 	if (it == con.end())
 		throw std::invalid_argument("There is not in this container");
